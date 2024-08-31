ce7ba871421cbc08d11b4a866f6d39054ca9c683
brunsilva-jc <brunsilva.jc@gmail.com>
2024-08-31 10:45:37 -0300

    implement code on main

--- main.py
+++ main.py
@@ -1,42 +1,74 @@
-from virtualenv.util.zipapp import extract
-
 menu = """
-    
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [q] Sair
+    [D] Deposit
+    [W] Withdraw
+    [S] Statement
+    [Q] Quit
 => """
 
-saldo = 0
-limite = 1000
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+balance = 0
+limit = 1000
+statement = ""
+withdraw_number = 0
+WITHDRAW_LIMIT = 5
 
 while True:
-    opcao = input(menu)
+    option = input(menu)
 
-    if opcao == "d":
-        print("Depósito")
+    if option == "D":
+        value = float(input("Enter the deposit amount: "))
 
-    elif opcao == "s":
-        print("Saque")
+        if value > 0:
+            balance += value
+            statement += f"Deposit: $ {value:.2f}\n"
+            print("======================================")
+            print(f"Successful Deposit: $ {value}".center(10))
+            print("======================================")
 
-    elif opcao == "e":
-        print("Extrato")
+        else:
+            print("Operation failed! The value is invalid")
 
-    elif opcao == 'q':
+    elif option == "W":
+        value = float(input("Enter the withdrawal amount:"))
+
+        exceeded_balance = value > balance
+        exceeded_limit = value > limit
+        exceeded_withdraw = withdraw_number >= WITHDRAW_LIMIT
+
+        if exceeded_balance:
+            print("Operation failed! You don't have enough balance!")
+
+        elif exceeded_limit:
+            print("Operation failed! Withdrawal amount exceeds limit")
+
+        elif exceeded_withdraw:
+            print("Operation failed! Maximum number of withdrawals exceeded")
+
+        elif value > 0:
+            balance -= value
+            statement += f"Withdrawal: $ {value:.2f}\n"
+            withdraw_number += 1
+            print("======================================")
+            print(f"Successful withdrawal: $ {value}".center(10))
+            print("======================================")
+
+        else:
+            print("Operation failed! The value is invalid")
+
+    elif option == "S":
+        print("\n============== STATEMENT ==============")
+        print("No transactions were made" if not statement else statement)
+        print(f"\nBalance: $ {balance:.2f}")
+        print("=======================================")
+
+
+    elif option == 'Q':
         break;
 
     else:
-        print("Operação inválida, selecione uma opção válida")
+        print("Invalid operation! select a valid option")
 
 
 
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
 
 
 
