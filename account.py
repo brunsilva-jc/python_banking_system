517c713a191677d329ae837623118387b2409fe4
brunsilva-jc <brunsilva.jc@gmail.com>
2024-09-02 11:07:35 -0300

    new classes created

---
+++ account.py
@@ -0,0 +1,5 @@
+class Account:
+    def __init__(self, agency, number, user):
+        self.agency = agency
+        self.number = number
+        self.user = user
---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, name, document, birth_date, address):
+        self.name = name
+        self.document = document
+        self.birth_date = birth_date
+        self.address = address
